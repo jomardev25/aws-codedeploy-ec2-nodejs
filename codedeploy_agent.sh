71750b28472e53a2a435c0e231dcbd5c759efb45
JomarIgn <jomar.ignacio@fujitsu.com>
2022-10-27 00:13:59 +0800

    Add codedeplo appspec

--- codedeploy_agent.sh
+++ codedeploy_agent.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # This installs the CodeDeploy agent and its prerequisites on Ubuntu 22.04.
-
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install ruby-full ruby-webrick wget -y
 cd /tmp
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/releases/codedeploy-agent_1.3.2-1902_all.deb
@@ -11,3 +10,15 @@ sed 's/Depends:.*/Depends:ruby3.0/' -i ./codedeploy-agent_1.3.2-1902_ubuntu22/DE
 dpkg-deb -b codedeploy-agent_1.3.2-1902_ubuntu22/
 sudo dpkg -i codedeploy-agent_1.3.2-1902_ubuntu22.deb
 systemctl list-units --type=service | grep codedeploy
+
+#!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install node v14.16.0
+n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
+sudo npm install pm2 -g
+git clone https://github.com/jomardev25/aws-codedeploy-ec2-nodejs.git
+cd aws-codedeploy-ec2-nodejs
+sudo npm install
+pm2 start index.js
---
+++ deployment_scripts/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/aws-codedeploy-ec2-nodejs
+pm2 start index.js -f
