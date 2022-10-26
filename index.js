219f85b54ef70d82ae8493474f508357dac7d3bd
JomarIgn <jomar.ignacio@fujitsu.com>
2022-10-26 17:40:06 +0800

    Add expresss

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res)=>{
+    res.status(200);
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({message: "Welcome to root URL of Server"}));
+});
+
+app.get('/health', (req, res)=>{
+    res.status(200);
+    res.setHeader("Content-Type", "application/json");
+    res.send("OK");
+});
+
+app.listen(PORT, (error) => {
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port " + PORT)
+    else 
+        console.error("Error occurred, server can't start", error);
+});
